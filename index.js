0b7676bf77f9473e5ad2c12e4dabf99dd9984228
Zachary Griffee <zacharygriffee@gmail.com>
2023-11-24 21:59:58 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,204 @@
+import RandomAccess from "random-access-storage";
+import {isFunction, isObject, once} from "./lib/dash.js";
+import b4a from "b4a";
+import {blocks} from "./lib/blocks.js";
+import {isNode} from "./lib/isNode.js";
+
+const DELIM = '\0';
+
+export function RandomAccessIndexedDB(dbname, xopts = {}) {
+    if (isNode()) {
+        throw new Error("Must be ran in browser");
+    }
+
+    const {
+        idb = window.indexedDB || getIdb()
+    } = xopts;
+
+    if (!idb) throw new Error('Browser does not support indexedDB.')
+
+    let db = null;
+    let dbqueue = [];
+
+    if (isFunction(idb.open)) {
+        const req = idb.open(dbname);
+        req.addEventListener('upgradeneeded', () => {
+            db = req.result
+            db.createObjectStore('data')
+        })
+        req.addEventListener('success', () => {
+            db = req.result
+            dbqueue.forEach(cb => { cb(db) })
+            dbqueue = null
+        })
+    } else {
+        db = idb
+    }
+
+    return (name, opts) => {
+        if (isObject(name)) {
+            opts = name
+            name = opts.name
+        }
+
+        if (!opts) opts = {}
+        opts.name = name
+
+        return new Store(Object.assign({ db: getdb }, xopts, opts))
+    }
+
+    function getdb (cb) {
+        if (db) setTimeout(() => cb(db))
+        else dbqueue.push(cb)
+    }
+}
+
+class Store extends RandomAccess {
+    constructor(opts = {}) {
+        const {
+            size = 4096,
+            name,
+            length = 0,
+            db
+        } = opts;
+
+        super();
+
+        this.size = size;
+        this.name = name;
+        this.length = length;
+        this._getdb = db;
+    }
+
+    _blocks(i, j) {
+        return blocks(this.size, i, j)
+    }
+
+    _read(req) {
+        const self = this;
+        const buffers = [];
+        self._store('readonly', (err, store) => {
+            if ((self.length || 0) < req.offset + req.size) {
+                return req.callback(new Error('Could not satisfy length'), null)
+            }
+            if (err) return req.callback(err)
+            const offsets = self._blocks(req.offset, req.offset + req.size);
+            let pending = offsets.length + 1;
+            const firstBlock = offsets.length > 0 ? offsets[0].block : 0;
+            for (let i = 0; i < offsets.length; i++) (o => {
+                const key = self.name + DELIM + o.block;
+                backify(store.get(key), (err, ev) => {
+                    if (err) return req.callback(err)
+                    buffers[o.block - firstBlock] = ev.target.result
+                        ? b4a.from(ev.target.result.subarray(o.start, o.end))
+                        : b4a.alloc(o.end - o.start)
+                    if (--pending === 0) req.callback(null, b4a.concat(buffers))
+                })
+            })(offsets[i])
+            if (--pending === 0) req.callback(null, b4a.concat(buffers))
+        })
+    }
+
+    _write(req) {
+        const self = this;
+
+        if (!b4a.isBuffer(req.data))
+            req.data = b4a.from(req.data);
+
+        self._store('readwrite', (err, store) => {
+            if (err) return req.callback(err)
+            const offsets = self._blocks(req.offset, req.offset + req.data.length);
+            let pending = 1;
+            const buffers = {};
+            for (let i = 0; i < offsets.length; i++) ((o, i) => {
+                if (o.end - o.start === self.size) return
+                pending++
+                const key = self.name + DELIM + o.block;
+                backify(store.get(key), (err, ev) => {
+                    if (err) return req.callback(err)
+                    buffers[i] = b4a.from(ev.target.result || b4a.alloc(self.size))
+                    if (--pending === 0) write(store, offsets, buffers)
+                })
+            })(offsets[i], i)
+            if (--pending === 0) write(store, offsets, buffers)
+        })
+
+        function write (store, offsets, buffers) {
+            let block;
+            let i = 0, j = 0;
+            for (; i < offsets.length; i++) {
+                const o = offsets[i];
+                const len = o.end - o.start;
+                if (len === self.size) {
+                    block = b4a.from(req.data.slice(j, j + len))
+                } else {
+                    block = buffers[i]
+                    b4a.copy(req.data, block, o.start, j, j + len);
+                }
+                store.put(block, self.name + DELIM + o.block)
+                j += len
+            }
+
+            const length = Math.max(self.length || 0, req.offset + req.data.length);
+            store.put(length, self.name + DELIM + 'length')
+            store.transaction.addEventListener('complete', () => {
+                self.length = length
+                req.callback(null)
+            })
+            store.transaction.addEventListener('error', err => {
+                req.callback(err)
+            })
+        }
+    }
+
+    _del(req) {
+        req.callback();
+    }
+
+    _store(mode, cb) {
+        cb = once(cb)
+        const self = this;
+        self._getdb(db => {
+            const tx = db.transaction(['data'], mode);
+            const store = tx.objectStore('data');
+            tx.addEventListener('error', cb)
+            cb(null, store)
+        })
+    }
+
+    _open(req) {
+        const self = this;
+        this._getdb(db => {
+            self._store('readonly', (err, store) => {
+                if (err) return req.callback(err)
+                backify(store.get(self.name + DELIM + 'length'), (err, ev) => {
+                    if (err) return req.callback(err)
+                    self.length = ev.target.result || 0
+                    req.callback(null)
+                })
+            })
+        })
+    }
+
+    _close(req) {
+        this._getdb(db => {
+            //db.close() // TODO: reopen gracefully. Close breaks with corestore, as innercorestore closes the db
+            req.callback()
+        })
+    }
+
+    _stat(req) {
+        const self = this;
+        setTimeout(
+            () => req.callback(null, { size: self.length })
+        );
+    }
+}
+
+function backify (r, cb) {
+    r.addEventListener('success', ev => { cb(null, ev) })
+    r.addEventListener('error', cb)
+}
+
+// Convenience
+export {RandomAccessIndexedDB as RAI, RandomAccessIndexedDB as default};
---
+++ readme.md
@@ -0,0 +1,70 @@
+# random-access-idb
+
+[random-access-storage][1] compatible indexedDB storage layer
+
+[1]: https://github.com/random-access-storage/random-access-storage
+
+### This is a modification of the original random-access-idb. 
+
+Differences:
+
+- Uses [`b4a`](https://www.npmjs.com/package/b4a) for buffer stuff.
+- Does not close the db after use. If your application is ephemeral it not really worrisome.
+- Modernize to ecmascript 6
+
+
+
+# example
+
+``` ecmascript 6
+import {RAI} from "@zacharygriffee/random-access-idb";
+// OR I've also added a default export.
+import RandomAccessIdb from "@zacharygriffee/random-access-idb";
+
+const random = RAI('dbname')
+    const cool = random('cool.txt')
+            cool.write(100, new Buffer('GREETINGS'), function (err) {
+        if (err) return console.error(err)
+            cool.read(104, 3, function (err, buf) {
+        if (err) return console.error(err)
+            console.log(buf.toString()) // TIN
+    })
+})
+```
+
+# api
+
+``` ecmascript 6
+import {RAI} from "@zacharygriffee/random-access-idb";
+```
+
+## const db = RAI(dbname, opts)
+
+Open an indexedDB database at `dbname`.
+
+    Any `opts` provided are forwarded to `db(name, opts)` as default options.
+
+## const file = db(name, opts)
+
+Create a handle `file` from `name` and `opts`:
+
+* `opts.size` - internal chunk size to use (default 4096)
+
+You must keep `opts.size` the same after you've written data.
+If you change the size, bad things will happen.
+
+## file.read(offset, length, cb)
+
+Read `length` bytes at an `offset` from `file` as `cb(err, buf)`.
+
+## file.write(offset, buf, cb)
+
+Write `buf` to `file` at an `offset`.
+
+# install
+
+npm install @zacharygriffee/random-access-idb
+
+# license
+
+BSD
