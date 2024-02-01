c618f18029145205da0e8f970821478e97330a15
Zachary Griffee <zacharygriffee@gmail.com>
2024-02-01 13:14:46 -0500

    - add process.platform to bundled

--- bundle.js
+++ bundle.js
@@ -5,12 +5,22 @@ import path from "node:path";
 
 import LocalDrive from "localdrive";
 import terser from "@rollup/plugin-terser";
+
 const p = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(p);
 
 const projectFolder = new LocalDrive(path.resolve(__dirname, "./"));
 
 await pack("./index.js", "./dist/index.min.js", {
+    output: {
+        banner() {
+            return `
+                globalThis.process = {
+                    platform: "browser"
+                };
+            `;
+        }
+    },
     plugins: [
         rollupFromSourcePlugin(projectFolder),
         commonjs(),
